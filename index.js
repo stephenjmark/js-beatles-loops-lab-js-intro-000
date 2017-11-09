8bd56d3f69c14f510645233d1607e2080f220017
Stephen Mark <stephenjmark@gmail.com>
2017-11-09 23:13:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 function theBeatlesPlay(musicians, instruments){
-var array = new Array();
-var arrayLength = musicians.length;
-let i = 0;
+  var array = new Array();
+  var arrayLength = musicians.length;
+  let i = 0;
 
-for (i; i < arrayLength; i++){
-  array[i] = `${musicians[i]} plays ${instruments[i]}`;
+  for (i; i < arrayLength; i++){
+      array[i] = `${musicians[i]} plays ${instruments[i]}`;
+    }
+
+  return array;
+
 }
 
-return array;
+function johnLennonFacts(facts){
+  var array = new Array();
+  var arrayLength = facts.length;
+  let i = 0;
 
+  while(i < arrayLength){
+    array[i] = `${facts[i]}!!!`;
+    i++
+  }
+
+  return array;
 }
